2b474f1e9590e1c24375b8bbeccaf87e93de082c
Sohailahamada <100353290+Sohailahamada@users.noreply.github.com>
2023-02-18 19:25:22 +0200

    home user update2

--- src/Component/Main/Main.js
+++ src/Component/Main/Main.js
@@ -4,7 +4,7 @@ import Navbar from '../HomeUser/Navbar/Navbar'
 import './Main.css'
 function Main() {
   return (
-       <div className="row">
+       <div className="row  rounded-3 p-3" style={{backgroundColor:'rgb(155, 193, 155)'}} >
         <div className="col-12 col-lg-10 p-4  rounded-4" style={{backgroundColor:'#fff',maxHeight:'93vh',overflowY:"scroll"}}>
           <Home />
         </div>
