33cfddbfc3042b706031d73540ec58bef9defbb1
mb4952144@gmail.com <mb4952144@gmail.com>
2023-02-20 18:25:48 +0200

    u

--- src/Component/Main/Main.js
+++ src/Component/Main/Main.js
@@ -9,7 +9,6 @@ function Main() {
         <div className="col-12 col-lg-10 py-4  px-3 homeContainer rounded-4" style={{backgroundColor:'#fff',overflowY:"scroll"}}>
           <ResponsiveNav />
           <Home />
-
         </div>
 
         <div className='col-2 d-none d-lg-flex' >
