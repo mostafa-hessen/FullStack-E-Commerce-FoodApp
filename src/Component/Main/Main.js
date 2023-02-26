0ea722617711dda41c35273f41fc8b113139d20b
mb4952144@gmail.com <mb4952144@gmail.com>
2023-02-26 11:34:27 +0200

    update

--- src/Component/Main/Main.js
+++ src/Component/Main/Main.js
@@ -3,7 +3,9 @@ import Home from '../HomeUser/Home/Home'
 import Navbar from '../HomeUser/Navbar/NavbarForUser'
 import ResponsiveNav from './ResponsiveNav/ResponsiveNav'
 import './Main.css'
-function Main() {
+import NavbarForUser from '../HomeUser/Navbar/NavbarForUser'
+import NavbarForCooker from '../../HomeForCookerAfterLogin/NavBarForCooker/NavBarForCooker'
+function Main(props) {
   return (
        <div className="row homNavContainer  rounded-3 " style={{backgroundColor:'rgb(155, 193, 155)'}} >
         <div className="col-12 col-lg-10 py-4  px-3 homeContainer rounded-4" style={{backgroundColor:'#fff',overflowY:"scroll"}}>
