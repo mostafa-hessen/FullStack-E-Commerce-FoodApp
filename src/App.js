310e46da42548e35cff4c3d092dad6b8a51af0b7
Takwa <takwaali1520@gmail.com>
2023-03-04 13:37:21 +0200

    takwa

--- src/App.js
+++ src/App.js
@@ -13,23 +13,10 @@ import Login from "./Component/Login"
 import PrivateRoute from "./Component/PrivateRoute"
 import ForgotPassword from "./Component/ForgotPassword"
 import UpdateProfile from "./Component/UpdateProfile"
-<<<<<<< HEAD
-import HomeAfterLoginForUser from "./HomeAfterLoginForUser/HomeAfterLoginForUser"
-function App() {
-  return (
-  
-       
-        <Router>
-          <AuthProvider>
-            <Switch>
-              <PrivateRoute path="/Home" component={HomeAfterLoginForUser} />
-              <PrivateRoute path="/update-profile" component={UpdateProfile} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/forgotpassword" component={ForgotPassword} />
-=======
 
-import logo from './logo.svg';
 import './App.css';
+import BasketHome from "./Component/Basket/Home";
+import Main from "./Component/Main/Main";
 
 function App() {
   return (
@@ -38,21 +25,17 @@ function App() {
         <Router>
           <AuthProvider>
             <Switch>
+           
               <PrivateRoute exact path="/HomeAfterLoginForUser" component={HomeAfterLoginForUser} />
               <PrivateRoute path="/update-profile" component={UpdateProfile} />
               <Route path="/signup" component={Signup} />
               <Route path="/forgot-password" component={ForgotPassword} />
->>>>>>> master
-              <Route path="/" component={Login} />
+              <Route path="/" component={Main} />
+              {/* <Route path="/" component={Login} /> */}
              
             </Switch>
           </AuthProvider>
         </Router>
-<<<<<<< HEAD
-        
-=======
->>>>>>> master
-  
   )
   }
 
--- src/Component/Main/Main.js
+++ src/Component/Main/Main.js
@@ -3,13 +3,20 @@ import Home from '../HomeUser/Home/Home'
 import Navbar from '../HomeUser/Navbar/Navbar'
 import ResponsiveNav from './ResponsiveNav/ResponsiveNav'
 import './Main.css'
+import HeaderComponent from '../shiefBage/ChiefProfileHeader'
+import InformationComponent from '../shiefBage/information'
+import RateChiefComponent from '../shiefBage/RateChiefComponent'
+import ChiefHome from '../shiefBage/chiefHome'
 function Main() {
   return (
        <div className="row homNavContainer  rounded-3 " style={{backgroundColor:'rgb(155, 193, 155)'}} >
         <div className="col-12 col-lg-10 py-4  px-3 homeContainer rounded-4" style={{backgroundColor:'#fff',overflowY:"scroll"}}>
-          <ResponsiveNav />
-          <Home />
-
+         {/*  <ResponsiveNav />
+          <Home /> */}
+{/* <HeaderComponent></HeaderComponent>
+<InformationComponent></InformationComponent>
+<RateChiefComponent></RateChiefComponent> */}
+<ChiefHome></ChiefHome>
         </div>
 
         <div className='col-2 d-none d-lg-flex' >
