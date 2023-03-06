48ba4adcc55731db8dcc5496d0b031cac8517671
Alaa-Khabeer <alaa.adel.khabeer@gmail.com>
2023-03-06 16:08:56 +0200

    semi-worked private route

--- src/App.js
+++ src/App.js
@@ -8,29 +8,50 @@ import Homebeforelogin from './HomeBeforeLogin/Homebeforelogin'
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomeForCookerAfterLogin from "./HomeForCookerAfterLogin/HomeForCookerAfterLogin";
 import SignPage from "./LoginSignupPopUp/SignPage";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
+
+import { Redirect } from 'react-router-dom'
+import { AuthContext} from "./contexts/AuthContext";
+
+
+
 function  App() {
   //let authFromSessionUser = setTimeout(sessionStorage.getItem('authorizeduser')?JSON.parse(sessionStorage.getItem('authorizeduser')):false,400)
  // let authFromSessionCooker = setTimeout(sessionStorage.getItem('authorizedcook')?JSON.parse(sessionStorage.getItem('authorizedcook')):false,400)
+
+const { currentUser } = useContext(AuthContext);
+
+ const ProtectedRoute = ({ children }) => {
+  if (!currentUser) {
+    return <Redirect to = '/'/>;
+  }
+
+  return children
+}; 
+
   return (
     <div className="App">
     <BrowserRouter>
       <Switch>
-          <Route exact path="/">
+          <Route exact path="/" component={() =>
             <Homebeforelogin />
-          </Route>
+          }/>
 
-          <Route path="/HomeUser" >
-          <HomeAfterLoginForUser  authorized={setTimeout(sessionStorage.getItem('authorizeduser')?JSON.parse(sessionStorage.getItem('authorizeduser')):false,400)}/>
-          </Route>
+          <Route path="/HomeUser" component={() =>
+          <ProtectedRoute>
+          <HomeAfterLoginForUser/>
+          </ProtectedRoute>
+          }/>
 
-         <Route path="/HomeCooker">
-            <HomeForCookerAfterLogin  authorized={setTimeout(sessionStorage.getItem('authorizedcook')?JSON.parse(sessionStorage.getItem('authorizedcook')):false,400)}  />
-         </Route>
+         <Route path="/HomeCooker" component={() =>
+         <ProtectedRoute>
+             <HomeForCookerAfterLogin />
+          </ProtectedRoute>
+          }/>
 
-          <Route path="/SignPage">
+         {/* <Route path="/SignPage">
            <SignPage/>
-          </Route>
+        </Route>*/}
           
         </Switch>
       </BrowserRouter>
