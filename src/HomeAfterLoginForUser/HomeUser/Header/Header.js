9442017a94d8a7370c52738e5f956fef8b53c1b9
Alaa-Khabeer <alaa.adel.khabeer@gmail.com>
2023-03-15 06:28:37 +0200

    client notification

--- src/HomeAfterLoginForUser/HomeUser/Header/Header.js
+++ src/HomeAfterLoginForUser/HomeUser/Header/Header.js
@@ -28,6 +28,7 @@
 // export default Header;
 
 import './Header.css'
+import Notification from './notification/notification'
 function Header(props) {
 
 
@@ -39,17 +40,14 @@ function Header(props) {
                 <div className="col-9 d-flex justify-content-between align-items-center">
                      <div className="image  d-flex align-items-center">
                            <img  className='d-lg-none' src="https://via.placeholder.com/50/09f/fff.png " alt="D" />
-                           <i className="notificationIcon ms-3 me-2 fa-regular fa-bell fa-lg "></i>
+                           <Notification/>
                      </div>
                     <div className="search-box d-flex align-items-center">
                         <div className="search-btn">
                             <i className="fas fa-search "></i>
                         </div>
                         <input type="text" placeholder="ابحث عن اكلتك" />
-                    </div>
-                    
-                   
-                    
+                    </div>               
                 </div>
 
                 <div className="col-3 text-center myHeaderText">
@@ -57,21 +55,8 @@ function Header(props) {
                     </p>
                     <p>{props.helloCon}</p>
                 </div>
-
-
-
-          
-
             </div>
-
-
-
-
         </>
     )
-
-
-
-
 }
 export default Header
